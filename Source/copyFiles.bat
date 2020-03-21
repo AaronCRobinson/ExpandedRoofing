50a3e64788d08600b16c7bf6d21a7783dbb54eab
Aaron C Robinson <robinson.aaron.c@gmail.com>
2020-03-21 20:10:17 +0800

    Clean-up

--- Source/copyFiles.bat
+++ Source/copyFiles.bat
@@ -1,13 +1,14 @@
 @echo off
 SET "ProjectName=ExpandedRoofing"
 SET "SolutionDir=C:\Users\robin\Desktop\Games\RimWorld Modding\Source\ExpandedRoofing\Source"
+SET "RWModsDir=D:\SteamLibrary\steamapps\common\RimWorld\Mods"
 @echo on
 
 del /S /Q "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\%ProjectName%\Defs\*"
 
-xcopy /S /Y "%SolutionDir%\..\About\*" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\%ProjectName%\About\"
-xcopy /S /Y "%SolutionDir%\..\Assemblies\*" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\%ProjectName%\Assemblies\"
-xcopy /S /Y "%SolutionDir%\..\Defs\*" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\%ProjectName%\Defs\"
-xcopy /S /Y "%SolutionDir%\..\Patches\*" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\%ProjectName%\Patches\"
-xcopy /S /Y "%SolutionDir%\..\Textures\*" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\%ProjectName%\Textures\"
-xcopy /S /Y "%SolutionDir%\..\Languages\*" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\%ProjectName%\Languages\"
+xcopy /S /Y "%SolutionDir%\..\About\*" "%RWModsDir%\%ProjectName%\About\"
+xcopy /S /Y "%SolutionDir%\..\Assemblies\*" "%RWModsDir%\%ProjectName%\Assemblies\"
+xcopy /S /Y "%SolutionDir%\..\Defs\*" "%RWModsDir%\%ProjectName%\Defs\"
+xcopy /S /Y "%SolutionDir%\..\Patches\*" "%RWModsDir%\%ProjectName%\Patches\"
+xcopy /S /Y "%SolutionDir%\..\Textures\*" "%RWModsDir%\%ProjectName%\Textures\"
+xcopy /S /Y "%SolutionDir%\..\Languages\*" "%RWModsDir%\%ProjectName%\Languages\"
